2faa62234c4f25a4aa07904b8c3209bccab993d6
LosAllen <lincolnjallen@gmail.com>
2025-03-01 16:35:11 -0600

    fixed swagger

--- models/task.js
+++ models/task.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
     taskName: { type: String, required: true },
-    taskDate: { type: Date, required: true },
-    completeTime: { type: Number, required: true },
+    taskDate: { type: String, required: true },
+    completeTime: { type: String, required: true },
     priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
     startTime: { type: String, required: true },
     endTime: { type: String, required: true }
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model("Task", taskSchema);
